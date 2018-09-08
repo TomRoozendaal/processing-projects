a82d1de2da1a9dde327ec517069d82dae859af1d
TomRoozendaal <t.v.roozendaal@student.tue.nl>
2018-09-08 16:08:58 +0200

    java build w/ processing builder from atom

--- build/source/cellularAutomata_1D.java
+++ build/source/cellularAutomata_1D.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class cellularAutomata_1D extends PApplet {
+
 /* Tom van Roozendaal
 *  A 1D Cellular Automata generator
 *  date: 08/09/2018
@@ -24,10 +40,10 @@ int rule = 120;
 boolean[][] grid;
 boolean[] previous;
 
-void setup() {
+public void setup() {
   grid = new boolean[stepsX][stepsY];
   previous = new boolean[3];
-  size(600, 300);
+  
   frameRate(30);
 
   for (int x = 0; x < grid.length; x++) {
@@ -65,7 +81,7 @@ void setup() {
   }
 }
 
-void draw() {
+public void draw() {
   background(0);
   noStroke();
   for (int y = 0; y < grid[0].length; y++) {
@@ -99,11 +115,11 @@ void draw() {
   text("S", 48, 2);
 }
 
-boolean randomBool() {
-  return random(1) > .5;
+public boolean randomBool() {
+  return random(1) > .5f;
 }
 
-void keyPressed(){
+public void keyPressed(){
   if (key == 's'){
     randomizeSides = !randomizeSides;
     setup();
@@ -122,7 +138,7 @@ void keyPressed(){
 }
 
 // Here's a function to setup own cellular automata rule
-boolean calcRule(boolean l, boolean c, boolean r){
+public boolean calcRule(boolean l, boolean c, boolean r){
   if (rule < 0 ){
 
     println(rule + " is not a valid rule input. Rule must be within [0, 255]");
@@ -145,4 +161,14 @@ boolean calcRule(boolean l, boolean c, boolean r){
           binaryNum.charAt(5) == '1' && !l && c &&! r ||
           binaryNum.charAt(6) == '1' && !l && !c && r ||
           binaryNum.charAt(7) == '1' && !l && !c && !r );
+}
+  public void settings() {  size(600, 300); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "cellularAutomata_1D" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
